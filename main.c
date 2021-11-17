de86ac97775c8951cdd6344eb34047bc2ad4265c
eric <eric.thomas@formation-industries-fc.fr>
2021-11-17 15:51:50 +0100

    menucloture

--- main.c
+++ main.c
@@ -13,6 +13,37 @@ int nvote=0;
 enum {CANDIDAT1=1,CANDIDAT2=2,CANDIDAT3=3,CANDIDAT4=4};
 bool scrutin=true; //scrutin ouvert
 int main() {
+    menuCloture();
+}
 
-    return 0;
+void menuCloture() {
+    int choix=0;
+    printf("Menu principal\n");
+    printf("1-Consulter les resultats\n");
+    printf("2-Afficher le nombre de votes\n");
+    scanf("%d",&choix);
+    switch (choix)
+    {
+        case 1:
+                affichageResultat();
+                break;
+        case 2:
+            nbvote();
+            break;
+
+        default:
+            printf("Erreur de saisie\n");
+            menuCloture();
+    }
+
+
+
+}
+
+void affichageResultat() {
+    printf("Affichage Résultat\n");
+}
+
+void nbvote() {
+    printf("Nbvote\n");
 }
