87d1f9b0bae6fe7e042a624f680de171ed970327
eric <eric.thomas@formation-industries-fc.fr>
2021-11-17 15:25:47 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/*************************prototypes*************************************/
+void menuCloture();
+void menu(); //menu scrutin ouvert
+void choixCandidat();
+void nbvote();
+void affichageResultat();
+/***********************variables globales*******************************/
+float vote1=0,vote2=0,vote3=0,vote4=0;
+int nvote=0;
+enum {CANDIDAT1=1,CANDIDAT2=2,CANDIDAT3=3,CANDIDAT4=4};
+bool scrutin=true; //scrutin ouvert
+int main() {
+
+    return 0;
+}
